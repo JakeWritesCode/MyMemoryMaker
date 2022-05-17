d6a9124ab32e138fbb2ef0650237449dea41dedf
jakesaunders <jakelsaunders94@gmail.com>
2022-05-17 16:00:56 +0100

    Settings

--- my_memory_maker/settings/production.py
+++ my_memory_maker/settings/production.py
@@ -3,7 +3,7 @@
 
 # 3rd-party
 import dj_database_url
-from base import *  # noqa: F403
+from my_memory_maker.settings.base import *  # noqa: F403
 
 DEBUG = getenv("DEBUG", False)  # noqa: F405
 ALLOWED_HOSTS = ["*"]
