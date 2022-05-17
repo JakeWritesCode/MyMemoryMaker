8d1e7d7f733712487fbaa13a3360e9c4b63b3e7a
jakesaunders <jakelsaunders94@gmail.com>
2022-05-17 16:13:32 +0100

    Static root

--- my_memory_maker/settings/production.py
+++ my_memory_maker/settings/production.py
@@ -6,7 +6,9 @@ import dj_database_url
 from my_memory_maker.settings.base import *  # noqa: F403
 
 DEBUG = getenv("DEBUG", False)  # noqa: F405
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = getenv("DJANGO_ALLOWED_HOSTS")
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = (BASE_DIR / "search/static/", BASE_DIR / "static")
 
 # DATABASES = {"default": dj_database_url.parse(getenv("DATABASE_URL"))}
 DATABASES = {
