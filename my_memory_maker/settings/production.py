136139add5a034f0006cca930207eb22f3849418
jakesaunders <jakelsaunders94@gmail.com>
2022-05-17 15:40:06 +0100

    Add dj-databse url

--- my_memory_maker/settings/production.py
+++ my_memory_maker/settings/production.py
@@ -2,7 +2,10 @@
 """Production settings."""
 
 # 3rd-party
+import dj_database_url
 from base import *  # noqa: F403
 
 DEBUG = getenv("DEBUG", False)  # noqa: F405
 ALLOWED_HOSTS = ["*"]
+
+DATABASES = {"default": dj_database_url.parse(getenv("DATABASE_URL"))}
