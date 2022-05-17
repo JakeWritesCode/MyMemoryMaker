01a9c9db294b5cfbdae53c2f4abf1a3d956aadaa
jakesaunders <jakelsaunders94@gmail.com>
2022-05-17 16:46:12 +0100

    Fix collectedstatic

--- my_memory_maker/settings/production.py
+++ my_memory_maker/settings/production.py
@@ -7,8 +7,6 @@ from my_memory_maker.settings.base import *  # noqa: F403
 
 DEBUG = getenv("DEBUG", False)  # noqa: F405
 ALLOWED_HOSTS = [getenv("DJANGO_ALLOWED_HOSTS")]
-STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = []
 
 # DATABASES = {"default": dj_database_url.parse(getenv("DATABASE_URL"))}
 DATABASES = {
