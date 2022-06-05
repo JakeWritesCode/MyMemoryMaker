ee2adb7f92757545a080796a3bbf010e3987736e
jakesaunders <jakelsaunders94@gmail.com>
2022-06-05 15:08:51 +0100

    Maybe it is

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # start-server.sh
-(cd /opt/app/MyMemoryMaker; gunicorn my_memory_maker.wsgi --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd /opt/app/MyMemoryMaker; gunicorn my_memory_maker.wsgi --worker-tmp-dir /dev/shm --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
