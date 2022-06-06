4ae00be58dccccb6555b2ed22c48db6877bac201
jakesaunders <jakelsaunders94@gmail.com>
2022-06-06 20:06:28 +0100

    Try again

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -2,7 +2,5 @@
 # start-server.sh
 (cd /opt/app/MyMemoryMaker; gunicorn my_memory_maker.wsgi --worker-tmp-dir /dev/shm --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
-chown -R celery:celery /var/run/celery
-chown -R celery:celery /var/log/celery
 /etc/init.d/celeryd start
 /etc/init.d/celerybeat start
