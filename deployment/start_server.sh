273ff25273858eea4125dfba535646a7eeeab99b
jakesaunders <jakelsaunders94@gmail.com>
2022-06-06 21:31:15 +0100

    Fuuuuccck

--- deployment/start_server.sh
+++ deployment/start_server.sh
@@ -3,5 +3,11 @@
 (cd /opt/app/MyMemoryMaker; gunicorn my_memory_maker.wsgi --worker-tmp-dir /dev/shm --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
 
+mkdir -p /var/run/celery
+mkdir -p /var/log/celery
+chown celery:celery /var/log/celery
+chown celery:celery /var/run/celery
+chmod g+w /var/run/celery
+chmod g+w /var/log/celery
 /etc/init.d/celeryd start
 /etc/init.d/celerybeat start
